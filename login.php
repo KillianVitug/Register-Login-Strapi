e8232878a1c7ae8c6b255f1c8931799e1c4acc49
Killian Vitug <killianjarelvitug@yahoo.com>
2022-11-21 16:32:27 +0800

    first Commit

---
+++ login.php
@@ -0,0 +1,21 @@
+<?php
+
+include "vendor/autoload.php";
+
+use App\AuthClient;
+
+$client = new AuthClient();
+
+$identifier = $_POST['identifier'];
+$password = $_POST['password'];
+
+$result = $client->login($identifier, $password);
+
+$body = $result->getBody()->getContents();
+$info = json_decode($body);
+$details = $info->user;
+
+$username = $details->username;
+$email = $details->email;
+
+header('Location: loginSuccess.php?username=' . $username . '&email=' . $email);
